fd4d33f5c8dcfda52ab403aec40faabdeec9c845
tejas1154 <tejas.ramane@aurionpro.com>
2025-06-30 09:16:59 +0530

    Add files via upload

---
+++ BookDiscount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookDiscount
+{
+    internal class BookDiscount
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+        public bool IsBestSeller { get; set; }
+
+        public BookDiscount(string title, string author, double price, bool isBestSeller)
+        {
+            Title = title;
+            Author = author;
+            Price = price;
+            IsBestSeller = isBestSeller;
+        }
+
+        public double CalculateDiscountedPrice()
+        {
+            double discountedPrice = Price;
+            if (IsBestSeller)
+                discountedPrice *= 0.9;
+
+            if (Price > 500)
+                discountedPrice *= 0.95;
+
+            return discountedPrice;
+        }
+
+        public void DisplayBookDetails()
+        {
+            Console.WriteLine($"Title: {Title}, Author: {Author}, Best Seller: {(IsBestSeller ? "Yes" : "No")}, Original Price; {Price}, Discounted Price: {CalculateDiscountedPrice()}");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace BookDiscount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BookDiscount book1 = new BookDiscount("The power of Habit", "Charles Duhigg", 600, true);
+            book1.DisplayBookDetails();
+            BookDiscount book2 = new BookDiscount("C# Basics", "Jane Doe", 450, false);
+            book2.DisplayBookDetails();
+        }
+    }
+}
